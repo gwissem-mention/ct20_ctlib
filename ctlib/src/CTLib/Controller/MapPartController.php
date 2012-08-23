45c88d2e6bb91f38706a45b42d129e80fd998792
Mike Turoff <mturoff@celltrak.com>
2012-08-23 03:51:33 +0000

    Added generic MapPartController (DynaPart)
    git-svn-id: https://hydra.ns.us.ctidata.net/svn/ct20_ctlib/trunk@3 c08e76d4-f78d-4ee6-81d2-a7b25414a72e

---
+++ ctlib/src/CTLib/Controller/MapPartController.php
@@ -0,0 +1,25 @@
+<?php
+namespace CTLib\Controller;
+
+use CTLib\Helper\LocalizationHelper;
+
+class MapPartController extends DynaPartController
+{
+    protected function addDynaPartDependencies($domId, $json, $assetLoader, $jsHelper)
+    {
+        $assetLoader
+            ->addExternalJs("http://www.mapquestapi.com/sdk/js/v7.0.s/mqa.toolkit.js?key=Gmjtd%7Clu6zn1ua2d%2C7s%3Do5-l07g0")
+            ->addAppJs("maps.plugin.js")
+            ->addAppCss("mapsPois.css")
+            ->addInlineJs('$("#'.$domId.'").maps('.$json.');');
+        $jsHelper
+            ->addConstants(
+                "CTLib\Helper\LocalizationHelper", "/^DISTANCE_UNIT/"
+            );
+    }
+
+    protected function buildDynaPartHtml($domAttributes)
+    {
+        return '<div ' . $this->compileDomAttributes($domAttributes) . '></div>';
+    }
+}
