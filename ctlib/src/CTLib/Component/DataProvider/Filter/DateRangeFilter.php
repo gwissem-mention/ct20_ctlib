b406244b666478ed4ab65044caa076032f0f07b0
sliu <sliu@celltrak.com>
2013-06-04 20:45:01 +0000

    added a type parameter to DateRangeFilter to make it to be able to filter on timestamp field
    git-svn-id: https://hydra.ns.us.ctidata.net/svn/ct20_ctlib/trunk@102 c08e76d4-f78d-4ee6-81d2-a7b25414a72e

--- ctlib/src/CTLib/Component/DataProvider/Filter/DateRangeFilter.php
+++ ctlib/src/CTLib/Component/DataProvider/Filter/DateRangeFilter.php
@@ -16,7 +16,10 @@ class DateRangeFilter implements DataProviderFilter
     const THIS_WEEK                 = 'thisWeek';
     const EARLIER_THAN_THIS_WEEK    = 'prevThisWeek';
     const SPECIFY                   = 'specify';
-    
+
+    const TYPE_DATETIME  = 'DATETIME';
+    const TYPE_TIMESTAMP = 'TIMESTAMP';
+
     /**
      * @var string
      */
@@ -27,15 +30,20 @@ class DateRangeFilter implements DataProviderFilter
      */
     protected $timezone;
 
+    /**
+     * @var string
+     */
+    protected $fieldType;
 
     /**
      * @param string $dateField
      * @param string $timezone
      */
-    public function __construct($dateField, $timezone=null)
+    public function __construct($dateField, $timezone, $filedType=DateRangeFilter::TYPE_DATETIME)
     {
         $this->dateField    = $dateField;
         $this->timezone     = $timezone;
+        $this->fieldType    = $filedType;
     }
 
     /**
@@ -49,46 +57,91 @@ class DateRangeFilter implements DataProviderFilter
             case self::TODAY:
                 $today = new \DateTime('today', $this->timezone);
                 $qbr->andWhere("{$this->dateField} BETWEEN :todayStart AND :todayEnd")
-                    ->setParameter('todayStart', $today->format('Y-m-d 00:00:00'))
-                    ->setParameter('todayEnd', $today->format('Y-m-d 23:59:59'));
+                    ->setParameter('todayStart', $this->formatStartTime($today))
+                    ->setParameter('todayEnd', $this->formatStopTime($today));
                 break;
 
             case self::YESTERDAY:
                 $yesterday = new \DateTime('yesterday', $this->timezone);
                 $qbr->andWhere("{$this->dateField} BETWEEN :yesterdayStart AND :yesterdayEnd")
-                    ->setParameter('yesterdayStart', $yesterday->format('Y-m-d 00:00:00'))
-                    ->setParameter('yesterdayEnd', $yesterday->format('Y-m-d 23:59:59'));
+                    ->setParameter('yesterdayStart', $this->formatStartTime($yesterday))
+                    ->setParameter('yesterdayEnd', $this->formatStopTime($yesterday));
                 break;
 
             case self::THIS_WEEK:
                 $today = new \DateTime('today', $this->timezone);
                 $weekStart = new \DateTime('Sunday last week', $this->timezone);
                 $qbr->andWhere("{$this->dateField} BETWEEN :weekStart AND :today")
-                    ->setParameter('weekStart', $weekStart->format('Y-m-d 00:00:00'))
-                    ->setParameter('today', $today->format('Y-m-d 23:59:59'));
+                    ->setParameter('weekStart', $this->formatStartTime($weekStart))
+                    ->setParameter('today', $this->formatStopTime($today));
                 break;
 
             case self::EARLIER_THAN_THIS_WEEK:
                 $weekStart = new \DateTime('Sunday last week', $this->timezone);
                 $qbr->andWhere("{$this->dateField} < :weekStart")
-                    ->setParameter('weekStart', $weekStart->format('Y-m-d 00:00:00'));
+                    ->setParameter('weekStart', $this->formatStartTime($weekStart));
                 break;
 
             case self::SPECIFY:
-                $dateFrom = Arr::mustGet('dateFrom', $value);
-                $dateTo   = Arr::mustGet('dateTo', $value);
+                $dateFrom = Arr::get('dateFrom', $value);
+                $dateTo   = Arr::get('dateTo', $value);
+                if (empty($dateFrom) || empty($dateTo)) {
+                    break;
+                }
                 $dateFromDateTime = new \DateTime($dateFrom["value"], $this->timezone);
                 $dateToDateTime   = new \DateTime($dateTo["value"], $this->timezone);
                 
                 // Use the passed range from and to dates.
                 $qbr->andWhere("{$this->dateField} BETWEEN :from AND :to")
-                    ->setParameter('from', $dateFromDateTime->format("Y-m-d 00:00:00"))
-                    ->setParameter('to', $dateToDateTime->format("Y-m-d 23:59:59"));
+                    ->setParameter('from', $this->formatStartTime($dateFromDateTime))
+                    ->setParameter('to', $this->formatStopTime($dateToDateTime));
                 break;
 
             default:
                 throw new \Exception("date can not be found");
         }
+    }
+
+    /**
+     * Format DateTime By Type
+     *
+     * @param DateTime $datetime This is a description
+     * @param string $type This is a description
+     * @return mixed 
+     *
+     */
+    protected function formatStartTime(\DateTime $datetime)
+    {
+        if ($this->fieldType == static::TYPE_DATETIME) {
+            return $datetime->format("Y-m-d 00:00:00");
+        }
+        
+        if ($this->fieldType == static::TYPE_TIMESTAMP) {
+            return $datetime->format("U");
+        }
+        
+        throw new \Exception("type can not be found");
+    }
+
+    /**
+     * Format DateTime By Type
+     *
+     * @param DateTime $datetime This is a description
+     * @param string $type This is a description
+     * @return mixed 
+     *
+     */
+    protected function formatStopTime(\DateTime $datetime)
+    {
+        if ($this->fieldType == static::TYPE_DATETIME) {
+            return $datetime->format("Y-m-d 23:59:59");
+        }
+        
+        if ($this->fieldType == static::TYPE_TIMESTAMP) {
+            return $datetime->format("U") + 24 * 3600 - 1;
+        }
+        
+        throw new \Exception("type can not be found");
     }
 
 }
