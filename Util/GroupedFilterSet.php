5023aa14f3999e8ae5207584b578f460a112ecb6
Mike Turoff <mturoff@celltrak.com>
2016-10-04 11:29:13 -0500

    DV-2254: Document class

--- Util/GroupedFilterSet.php
+++ Util/GroupedFilterSet.php
@@ -1,14 +1,28 @@
 <?php
 namespace CTLib\Util;
 
-
-class GroupedFilterSet implements \Iterator, \Countable
+/**
+ * Collection of grouped filters.
+ * @author Mike Turoff
+ */
+class GroupedFilterSet implements \Iterator, \Countable, \JsonSerializable
 {
-
+    /**
+     * Maintains iterator position.
+     * @var integer
+     */
     protected $index;
 
+    /**
+     * Set of unique filter groups.
+     * @var array
+     */
     protected $filterGroups;
 
+    /**
+     * Set of filter sets (one per group).
+     * @var array
+     */
     protected $filters;
 
     public function __construct()
@@ -18,6 +32,13 @@ class GroupedFilterSet implements \Iterator, \Countable
         $this->filters = [];
     }
 
+    /**
+     * Adds filters for specified group. Will append filters if group already
+     * exists.
+     * @param mixed $filterGroup
+     * @param mixed $filters Can add individual or multiple.
+     * @return GroupedFilterSet
+     */
     public function addFilters($filterGroup, $filters)
     {
         $filters = (array) $filters;
@@ -33,41 +54,70 @@ class GroupedFilterSet implements \Iterator, \Countable
         return $this;
     }
 
-    public function getFilters()
+    /**
+     * Returns grouped filters as array.
+     * @return array [filterGroupId => [filterId, ...], ...]
+     */
+    public function getGroupedFilters()
     {
         return array_combine($this->filterGroups, $this->filters);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function current()
     {
         return $this->filters[$this->index];
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function key()
     {
         return $this->filterGroups[$this->index];
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function next()
     {
         ++$this->index;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function rewind()
     {
         $this->index = 0;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function valid()
     {
         return isset($this->filterGroups[$this->index]);
     }
 
+    /**
+     * Returns the number of unique filter groups in collection.
+     * @return integer
+     */
     public function count()
     {
         return count($this->filterGroups);
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
+    public function jsonSerialize()
+    {
+        return $this->getGroupedFilters();
+    }
 
 }
