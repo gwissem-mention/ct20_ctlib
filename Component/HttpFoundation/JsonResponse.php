46cedd69dabd52624def193e30967881967e35ba
Mike Turoff <mturoff@celltrak.com>
2016-12-12 19:37:44 -0600

    DV-2853: Log json_encode error

--- Component/HttpFoundation/JsonResponse.php
+++ Component/HttpFoundation/JsonResponse.php
@@ -20,22 +20,34 @@ class JsonResponse extends \Symfony\Component\HttpFoundation\Response
      *
      * @param mixed   $content
      * @param integer $httpResponseCode
+     * @param array   $headers
      */
-    public function __construct($content, $httpResponseCode=200, $headers=array())
-    {
-        if (! is_string($content)) {
+    public function __construct(
+        $content,
+        $httpResponseCode = 200,
+        $headers = []
+    ) {
+        if (!is_string($content)) {
             if (is_object($content) && method_exists($content, 'toJson')) {
-                $content = $content->toJson();
+                // Using custom toJson method. This dates back from when we
+                // didn't have access to JsonSerializable interface. The use of
+                // #toJson is deprecated; implement JsonSerializable instead.
+                $json = $content->toJson();
             } else {
-                $content = json_encode($content);
+                $json = json_encode($content);
+
+                if ($json === false) {
+                    $errorCode  = json_last_error();
+                    $errorMsg   = json_last_error_msg();
+                    $content    = print_r($content, true);
+                    throw new \RuntimeException("Failed to JSON-encode response content with error [{$errorCode}] '{$errorMsg}'.\n\n{$content}");
+                }
             }
         }
 
-        parent::__construct(
-            $content,
-            $httpResponseCode,
-            $headers + array('Content-type' => 'application/json')
-        );
+        $headers['Content-type'] = 'application/json';
+
+        parent::__construct($json, $httpResponseCode, $headers);
     }
 
 }
