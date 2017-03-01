18e54c4dcbf21a25eb512d953df690d41119c869
dmclean <dmclean@celltrak.com>
2017-03-01 18:09:50 -0500

    Support for AND operator

--- Component/DataAccess/CtApiDocumentDataAccess.php
+++ Component/DataAccess/CtApiDocumentDataAccess.php
@@ -147,8 +147,8 @@ class CtApiDocumentDataAccess implements DataAccessInterface
         }
 
         if (is_array($value)) {
-            if (!in_array($operator, ['eq', 'in', "notIn"])) {
-                throw new \InvalidArgumentException("Array value only supports 'eq' or 'in' or 'notIn' operator.");
+            if (!in_array($operator, ['eq', 'in', 'notIn', 'and'])) {
+                throw new \InvalidArgumentException("Array value only supports 'eq' or 'in' or 'notIn' or 'and' operators.");
             }
             if ($operator == 'eq') {
                 $operator = 'in';
